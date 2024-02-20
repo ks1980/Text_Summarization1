880b804141c28d2bb4fff801871744f9b7a3973d
ks1980 <51252953+ks1980@users.noreply.github.com>
2024-02-21 00:50:51 +0530

    Update script.py

--- script.py
+++ script.py
@@ -28,7 +28,7 @@ result = []
 with st.form('summarize_form', clear_on_submit=True):
     openai_api_key = st.text_input('OpenAI API Key', type = 'password', disabled=not txt_input)
     submitted = st.form_submit_button('Submit')
-    if submitted and openai_api_key.startswith('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'):
+    if submitted and openai_api_key.startswith('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'):
         with st.spinner('Calculating...'):
             response = generate_response(txt_input)
             result.append(response)
